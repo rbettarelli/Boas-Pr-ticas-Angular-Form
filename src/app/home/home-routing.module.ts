61801bf5756bd5846025aad5789da1747e98ceaa
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2023-01-05 23:54:58 -0300

    Template Cadastro

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,3 +1,4 @@
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -7,7 +8,8 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    children: [{ path: '', component: LoginComponent }],
+    children: [{ path: '', component: LoginComponent },
+    {path: 'novousuario', component: NovoUsuarioComponent}],
   },
 ];
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,10 +7,11 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, LoginComponent],
+  declarations: [HomeComponent, LoginComponent, NovoUsuarioComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
