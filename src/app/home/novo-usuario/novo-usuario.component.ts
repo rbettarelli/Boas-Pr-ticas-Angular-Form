8ee5dda134f178677e0223c06fe9a417b5113a36
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2023-01-06 22:25:50 -0300

    validação padrao de formulario

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,6 +1,6 @@
 import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
 
 @Component({
@@ -18,10 +18,10 @@ export class NovoUsuarioComponent implements OnInit {
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      email: [''],
-      fullName: [''],
-      userName: [''],
-      password: [''],
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['',[Validators.required]],
+      userName: ['',[Validators.required]],
+      password: ['', [Validators.required]],
     });
   }
   cadastrar() {
