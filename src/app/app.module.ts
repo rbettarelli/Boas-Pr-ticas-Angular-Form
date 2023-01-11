247e107f639dff8d0adce7d3eaee578f9745b5ef
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2023-01-11 02:02:27 -0300

    Projeto finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,24 @@
-import { RodapeModule } from './componentes/rodape/rodape.module';
-import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AutenticacaoComponent } from './autenticacao/autenticacao.component';
 import { HttpClientModule } from '@angular/common/http';
-import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
+import { RodapeModule } from './componentes/rodape/rodape.module';
 import { AutenticacaoModule } from './autenticacao/autenticacao.module';
-import { MenuComponent } from './componentes/menu/menu.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuComponent
-
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule, CabecalhoModule, RodapeModule, AutenticacaoModule
-
-
+    HttpClientModule,
+    CabecalhoModule,
+    RodapeModule,
+    AutenticacaoModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/componentes/menu/menu.module.ts
+++ src/app/componentes/menu/menu.module.ts
@@ -1,12 +1,11 @@
+import { CabecalhoModule } from './../cabecalho/cabecalho.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MenuComponent } from './menu.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [MenuComponent],
+  imports: [CommonModule],
+  exports: [MenuComponent],
 })
-export class MenuModule { }
+export class MenuModule {}
